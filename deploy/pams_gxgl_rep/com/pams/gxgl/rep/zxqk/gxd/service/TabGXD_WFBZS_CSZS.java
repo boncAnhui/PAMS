ff9d63b15ad649dc12a455b279917687e6c0dc02
skynetbird <skynetbird@126.com>
2014-09-15 23:01:07 +0800

    commit

---
+++ deploy/pams_gxgl_rep/com/pams/gxgl/rep/zxqk/gxd/service/TabGXD_WFBZS_CSZS.java
@@ -0,0 +1,52 @@
+package com.pams.gxgl.rep.zxqk.gxd.service;
+
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.headray.core.spring.jdo.DyDaoHelper;
+import com.headray.framework.services.db.dybeans.DynamicObject;
+import com.pams.gxgl.rep.helper.ZXQKHelper;
+import com.ray.nwpn.itsm.report.common.RepHelper;
+
+@Component
+@Transactional
+public class TabGXD_WFBZS_CSZS
+{
+	JdbcTemplate jt;
+
+	public List execute(DynamicObject obj) throws Exception
+	{
+		String begindate = obj.getFormatAttr("begindate");
+		String enddate = obj.getFormatAttr("enddate");
+		String ownerctx = obj.getFormatAttr("ownerctx");
+		
+		String sql_cdate = RepHelper.compare_sysdate(enddate);		
+
+		obj.setAttr("sql_cdate", sql_cdate);
+		obj.setAttr("ispublish", "N");
+		obj.setAttr("isnodeovertime", "Y");
+		obj.setAttr("ownerctx", ownerctx);
+		
+		StringBuffer sql = new StringBuffer();
+
+		sql.append(ZXQKHelper.sql_xxgx_zxqk_zxsc(obj));
+		
+		List datas = DyDaoHelper.query(jt, sql.toString());
+
+		return datas;
+	}
+
+	public void setJdbcTemplate(JdbcTemplate jt)
+	{
+		this.jt = jt;
+	}
+
+	public JdbcTemplate getJdbcTemplate()
+	{
+		return this.jt;
+	}
+
+}
