21de8ff3a4ed3ab138625e99262b15b171434ff8
skynetbird <skynetbird@126.com>
2014-09-24 21:29:50 +0800

    导出报表

--- deploy/pams_gxgl_kpi/com/pams/gxgl/kpi/jsl/action/RepAction.java
+++ deploy/pams_gxgl_kpi/com/pams/gxgl/kpi/jsl/action/RepAction.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.blue.ssh.core.action.SimpleAction;
 import com.blue.ssh.core.utils.web.struts2.Struts2Utils;
 import com.headray.framework.services.db.dybeans.DynamicObject;
 import com.headray.framework.spec.GlobalConstants;
@@ -13,8 +12,9 @@ import com.pams.gxgl.kpi.jsl.service.TabJSL_GS;
 import com.pams.gxgl.kpi.jsl.service.TabJSL_RY;
 import com.ray.app.chart.report.dao.ReportDao;
 import com.ray.app.query.service.QueryService;
+import com.ray.nwpn.itsm.report.common.ReportAction;
 
-public class RepAction extends SimpleAction
+public class RepAction extends ReportAction
 {
 	private String _searchname;
 
@@ -83,6 +83,32 @@ public class RepAction extends SimpleAction
 		arg.put("enddate", enddate);
 		arg.put("internal", internal);
 		return "tab_jsl_gs";
+	}
+	
+	public String xls_jsl_gs() throws Exception
+	{
+		String begindate = Struts2Utils.getRequest().getParameter("begindate");
+		String enddate = Struts2Utils.getRequest().getParameter("enddate");
+		String internal = Struts2Utils.getRequest().getParameter("internal");
+		
+		DynamicObject obj = new DynamicObject();
+		obj.setAttr("begindate", begindate);
+		obj.setAttr("enddate", enddate);
+		obj.setAttr("internal", internal);
+		
+		//发布总数
+		TabJSL_GS tabJSL_GS = new TabJSL_GS();
+		tabJSL_GS.setJdbcTemplate(reportDao.getJdbcTemplate());
+		
+		List datas = tabJSL_GS.execute(obj);
+		
+		String[] cnames = new String[]{ "公司", "考核总分" };
+		String[] pnames = new String[]{ "orgcname", "zxsccskh" };
+		String fileName = "共享及时率考核公司统计.xls";
+
+		exp_excel(cnames, pnames, datas, fileName);
+
+		return "excel";
 	}	
 	
 	
@@ -136,6 +162,32 @@ public class RepAction extends SimpleAction
 		return "tab_jsl_bm";
 	}
 	
+	public String xls_jsl_bm() throws Exception
+	{
+		String begindate = Struts2Utils.getRequest().getParameter("begindate");
+		String enddate = Struts2Utils.getRequest().getParameter("enddate");
+		String internal = Struts2Utils.getRequest().getParameter("internal");
+		
+		DynamicObject obj = new DynamicObject();
+		obj.setAttr("begindate", begindate);
+		obj.setAttr("enddate", enddate);
+		obj.setAttr("internal", internal);
+		
+		//发布总数
+		TabJSL_BM tabJSL_BM = new TabJSL_BM();
+		tabJSL_BM.setJdbcTemplate(reportDao.getJdbcTemplate());
+		
+		List datas = tabJSL_BM.execute(obj);
+
+		String[] cnames = new String[]{ "部门", "考核总分" };
+		String[] pnames = new String[]{ "orgcname", "zxsccskh" };
+		String fileName = "共享及时率考核部门统计.xls";
+
+		exp_excel(cnames, pnames, datas, fileName);
+
+		return "excel";
+	}	
+	
 	public String main_jsl_ry() throws Exception
 	{
 		String begindate = Struts2Utils.getRequest().getParameter("begindate");
@@ -187,6 +239,33 @@ public class RepAction extends SimpleAction
 		arg.put("internal", internal);
 		return "tab_jsl_ry";
 	}	
+	
+	
+	public String xls_jsl_ry() throws Exception
+	{
+		String begindate = Struts2Utils.getRequest().getParameter("begindate");
+		String enddate = Struts2Utils.getRequest().getParameter("enddate");
+		String internal = Struts2Utils.getRequest().getParameter("internal");
+		
+		DynamicObject obj = new DynamicObject();
+		obj.setAttr("begindate", begindate);
+		obj.setAttr("enddate", enddate);
+		obj.setAttr("internal", internal);
+		
+		//发布总数
+		TabJSL_RY tabJSL_RY = new TabJSL_RY();
+		tabJSL_RY.setJdbcTemplate(reportDao.getJdbcTemplate());
+		
+		List datas = tabJSL_RY.execute(obj);
+
+		String[] cnames = new String[]{ "人员", "考核总分", "所属公司", "所属部门"};
+		String[] pnames = new String[]{ "username", "zxsccskh", "orgcname", "deptcname" };
+		String fileName = "共享及时率考核人员统计.xls";
+
+		exp_excel(cnames, pnames, datas, fileName);
+
+		return "excel";
+	}
 
 
 }
